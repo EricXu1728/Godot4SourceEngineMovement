5115fafaac9562aed7097f27dd0f15bda0ebcfd1
Eric Xu <ericxu1728@gmail.com>
2023-10-21 13:33:21 -0400

    Funny spinning icon

---
+++ Sprite3D.gd
@@ -0,0 +1,15 @@
+extends Sprite3D
+
+const SPEED = 2
+var coins = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(deg_to_rad(SPEED))
+	pass
