663b9a35c533ebcc18e9a2b0e2509b4fcade5aff
Eric Xu <ericxu1728@gmail.com>
2023-12-06 11:04:49 -0500

    oopsie

--- tscns/Standing.cs
+++ tscns/Standing.cs
@@ -1,23 +1,15 @@
 using Godot;
 using System;
 
-
-
-public partial class new_script : Node
+public partial class Standing : Node
 {
-
-	[Export] public playerVariables.player_vars stats;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//GD.Print("wow");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//GD.Print("wow2");
-		//GD.Print(stats.vel);
-		//GD.Print("wow3");
 	}
 }
