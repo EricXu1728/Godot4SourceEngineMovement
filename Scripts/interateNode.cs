a2427ec6f7ecf7fcc63d675b8e434c69569e0a28
Eric Xu <ericxu1728@gmail.com>
2024-02-04 12:23:16 -0500

    Added ability to iterate a node

--- Scripts/interateNode.cs
+++ Scripts/interateNode.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 [Tool]
 
-public partial class interateNode : Node
+public partial class interateNode : Node3D
 {
 	private Boolean _build = false;
+
+	[Export] Vector3 itemSpread = Vector3.One;
+	[Export] Vector3 itemSize = Vector3.One;
+	[Export] Vector3 itemScale = Vector3.One;
 	[Export] Boolean Build
 	{
 		get => false;
@@ -15,19 +20,57 @@ public partial class interateNode : Node
 			if (Engine.IsEditorHint() && value != Build){
 				GD.Print("Building?");
 				//GD.Print(distance);
-				//spawnNodes();
+				spawnNodes();
 			}
 		}
 	}
 	
+	private PackedScene instancePackedScene;
+	private String _file_path = "";
+	[Export(PropertyHint.File)] public String file_path
+	{
+		get => _file_path;
+		set
+		{
+			_file_path = value;
+			instancePackedScene = (PackedScene)ResourceLoader.Load(_file_path);
+		}
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print(file_path);
+		spawnNodes();
+	}
+
+
+	public void spawnNodes(){
+		GD.Print("spawining");
+		foreach(Node3D n in listOfNodes){
+			n.QueueFree();
+		}
+		listOfNodes.Clear();
+
+		for (int x = 0; x < itemSpread[0]; x++){
+			for (int y = 0; y < itemSpread[1]; y++){
+				for (int z = 0; z < itemSpread[2]; z++){
+					addNode(new Vector3(x*itemSize[0], y * itemSize[1], z * itemSize[2]), itemScale);
+				}
+			}
+		}
 		
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+
+	private List<Node3D> listOfNodes = new List<Node3D>(); 
+	public Node3D addNode(Vector3 position, Vector3 scale){
+		Node3D newNode = (Node3D)instancePackedScene.Instantiate();
+		AddChild(newNode);
+		newNode.Scale = scale;
+		newNode.Position = position;
+		listOfNodes.Add(newNode);
+		GD.Print("node added");
+		return newNode;
 	}
 }
