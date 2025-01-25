9497c110e6c9adebcd579054e559589371556d55
EricXu1728 <ericxu1728@gmail.com>
2025-01-25 14:05:35 -0500

    Added ebucario's fix #9 to crouching
    fix stuck-crouch bug #9

--- Scripts/playerMovementScripts/Crouched.cs
+++ Scripts/playerMovementScripts/Crouched.cs
@@ -1,46 +1,45 @@
-
 using System;
 using Godot;
 
-namespace StateMachine;
-public partial class Crouched : playerState
+namespace StateMachine
 {
-	 
-	public Boolean tryUncrouch = false;
-	
-	public override void Enter(String msg)
-	{  
-		GD.Print("crouched");
-		stats.crouched = true;
-		stats.speed = stats.ply_crouchspeed ;
-		tryUncrouch = false;
-		
-		
+	public partial class Crouched : playerState
+	{
+		public bool tryUncrouch = false;
+
+		public override void Enter(string msg)
+		{
+			GD.Print("crouched");
+			stats.crouched = true;
+			stats.speed = stats.ply_crouchspeed;
+
+			// Check for "try_uncrouch" in the message and update tryUncrouch
+			if (msg.Contains("try_uncrouch"))
+			{
+				tryUncrouch = true;
+			}
+			else
+			{
+				tryUncrouch = false;
+			}
+		}
+
+		public override void PhysicsUpdate(double delta)
+		{
+			if (Input.IsActionJustReleased("crouch"))
+			{
+				tryUncrouch = true;
+			}
+
+			if (tryUncrouch && !player.bonker.IsColliding())
+			{
+				ownerStateMachine.TransitionTo("Standing");
+			}
+
+			if (player.bonker.IsColliding())
+			{
+				GD.Print("bonking");
+			}
+		}
 	}
-	
-	public override void PhysicsUpdate(double delta)
-	{  
-	
-		if(Input.IsActionJustReleased("crouch"))
-		{
-			tryUncrouch = true;
-			
-			
-		}
-		if(tryUncrouch == true && player.bonker.IsColliding() == false)
-		{
-	
-			ownerStateMachine.TransitionTo("Standing");
-			
-		}
-		if(player.bonker.IsColliding())
-		{
-			GD.Print("bonking");
-	
-	
-		}
-	}
-	
-	
-	
 }
--- Scripts/playerMovementScripts/Crouching.cs
+++ Scripts/playerMovementScripts/Crouching.cs
@@ -1,62 +1,54 @@
-
 using System;
 using Godot;
 
-namespace StateMachine;
-
-public partial class Crouching : playerState
+namespace StateMachine
 {
-	 
-	public Boolean tryUncrouch = false;
-	
-	public override void Enter(String msg)
-	{  
-		GD.Print("crouching");
-		stats.crouching = true;
-		tryUncrouch = false;
-		stats.speed = stats.ply_crouchspeed ;
-		
-	}
-	
-	public override void PhysicsUpdate(double delta)
-	{  
-		if(Input.IsActionJustReleased("crouch"))
+	public partial class Crouching : playerState
+	{
+		public bool tryUncrouch = false;
+
+		public override void Enter(string msg)
 		{
-			tryUncrouch = true;
-			
-		}
-		if(tryUncrouch == true && player.bonker.IsColliding() == false)
-		{
-	
-			ownerStateMachine.TransitionTo("Standing");
-			
-			
+			GD.Print("crouching");
+			stats.crouching = true;
+			tryUncrouch = false;
+			stats.speed = stats.ply_crouchspeed;
 		}
 
-		Vector3 newScale = player.myShape.Scale;
-		newScale[1] -=0.1f;
-		newScale[1] = (float)Mathf.Clamp(newScale[1], 0.1, 1);
+		public override void PhysicsUpdate(double delta)
+		{
+			if (Input.IsActionJustReleased("crouch"))
+			{
+				tryUncrouch = true;
+			}
 
-		player.myShape.Scale = newScale;
-		player.mySkin.Scale = newScale;
-		
-		
-		if(stats.on_floor)
-		{
-			player.MoveAndCollide(new Vector3(0,-0.1f, 0));
+			if (tryUncrouch && !player.bonker.IsColliding())
+			{
+				ownerStateMachine.TransitionTo("Standing");
+			}
+
+			Vector3 newScale = player.myShape.Scale;
+			newScale[1] -= 0.1f;
+			newScale[1] = (float)Mathf.Clamp(newScale[1], 0.1, 1);
+
+			player.myShape.Scale = newScale;
+			player.mySkin.Scale = newScale;
+
+			if (stats.on_floor)
+			{
+				player.MoveAndCollide(new Vector3(0, -0.1f, 0));
+			}
+			else
+			{
+				player.MoveAndCollide(new Vector3(0, 0.1f, 0));
+			}
+
+			// Pass tryUncrouch when transitioning to "Crouched"
+			if (newScale[1] <= 0.5f)
+			{
+				string msg = tryUncrouch ? "try_uncrouch=true" : "";
+				ownerStateMachine.TransitionTo("Crouched", msg);
+			}
 		}
-		else
-		{
-			player.MoveAndCollide(new Vector3(0,0.1f, 0));
-		
-		}
-		if(newScale[1] <=0.5){
-			ownerStateMachine.TransitionTo("Crouched");
-		}
-	
-	
 	}
-	
-	
-	
 }
